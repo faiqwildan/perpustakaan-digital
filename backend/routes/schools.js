25d153d57598974c99e7bba97f84e95cc4c176fe
faiqwildan <faiqwildan88@gmail.com>
2026-05-26 20:07:19 +0700

    Update schools.js

--- backend/routes/schools.js
+++ backend/routes/schools.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router  = express.Router();
-const path    = require('path');
-const fs      = require('fs');
 const multer  = require('multer');
 const db      = require('../config/db');
 const supabase = require('../config/supabase');
